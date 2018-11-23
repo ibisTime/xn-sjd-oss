e067a99b7c412e24432cc723c85936cf2cc29636
kylong <1603049109@qq.com>
2018-11-23 10:24:52 +0800

    k

--- src/container/home/seller/seller.js
+++ src/container/home/seller/seller.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Row, Col } from 'antd';
+import { Row, Col, Avatar } from 'antd';
 import Kyyjje from '../kyyjje/kyyjje';
 import Txclz from '../txclz/txclz';
 import Multiple from '../multiple/multiple';
